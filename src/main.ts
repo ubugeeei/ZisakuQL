acf30aea1d0fb0b544aa4f1f1ceb92f9e4c1807d
Ubugeeei <ubuge1122@gamil.com>
2022-12-02 23:31:23 +0900

    feat: simple query

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { serve } from "https://deno.land/std@0.167.0/http/server.ts";
 import { Hono } from "https://deno.land/x/hono@v2.5.7/mod.ts";
-import { resolve } from "./query_manager/index.ts";
+import { resolveQuery } from "./resolve_query/index.ts";
 
 const app = new Hono();
-app.get("/zisakuql", (c) => {
-	const resObject = resolve(c.body);
-	return c.json(resObject);
+
+app.post("/zisakuql", async (c) => {
+	const requestBody = await c.req.text();
+
+	const res = resolveQuery(requestBody);
+
+	return c.json(res);
 });
+
 serve(app.fetch);
