3807b1b48bec130cfb75ea4922e5ff970422bc62
Ubugeeei <ubuge1122@gamil.com>
2022-12-03 01:32:39 +0900

    feat: define tokens

---
+++ src/resolve_query/token.ts
@@ -0,0 +1,15 @@
+export type Token = {
+	type: TokenType;
+	literal: string;
+};
+
+export enum TokenType {
+	Query,
+	Identifier,
+  Number,
+	LeftParen,
+	RightParen,
+	LeftBrace,
+	RightBrace,
+	Colon,
+}
